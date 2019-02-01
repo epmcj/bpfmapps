ba553db0fa66d9d0ef42b1ee73da4961237dfc5a
epmcj <epmcj@hotmail.com>
2019-02-01 09:44:07 -0200

    new version of cmin sketch (now uses own hashing method)

--- bpfmap/cmin_sketch[OLD].c
+++ bpfmap/cmin_sketch[OLD].c
@@ -3,8 +3,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include <errno.h>
-#include <stdio.h>
 
+#include "libghthash/ght_hash_table.h"
 #include "cmin_sketch.h"
 #include "bpfmap.h"
 
@@ -16,34 +16,18 @@ struct cmin_elem {
 
 struct bpf_cmintab {
     struct bpf_map map;
-    // void *elems;
+    void *elems;
 
-    counter_t** cmin_table;
+    ght_hash_table_t** cmintab;
+    ght_iterator_t iterator;
+    struct cmin_elem *current;
+
     uint16_t num_rows;
     uint16_t num_columns;
 
-    uint8_t* as;
-    uint8_t* bs;
-
     uint32_t n_buckets;
-    // uint32_t elem_size;
+    uint32_t elem_size;
 };
-
-
-uint32_t hash(uint8_t a, uint8_t b, void* key, uint32_t key_size) {
-    uint32_t hvalue = 0;
-    uint32_t i;
-
-    for (i = 0; i < key_size; i++) {
-        hvalue += (uint32_t) (((uint8_t *)key)[i]);
-        hvalue += (hvalue << a);
-        hvalue ^= (hvalue >> b);
-    }
-    hvalue += (hvalue << (b - a));
-    hvalue ^= (hvalue >> (a - b));
-    hvalue += (hvalue << (a + b));
-    return hvalue;
-}
 
 /** value_size must be equals to 4 (bytes) because we suppose the use of 
  ** uint32_t counters.
@@ -66,52 +50,35 @@ struct bpf_map *cmin_map_alloc(union bpf_attr *attr) {
     }
 
     /* mandatory map attributes */
-    cmintab->map.map_type    = attr->map_type;
-    cmintab->map.key_size    = attr->key_size;
-    cmintab->map.value_size  = attr->value_size;
+    cmintab->map.map_type = attr->map_type;
+    cmintab->map.key_size = attr->key_size;
+    cmintab->map.value_size = attr->value_size;
     cmintab->map.max_entries = attr->max_entries;
-    cmintab->map.map_flags   = attr->map_flags;
+    cmintab->map.map_flags = attr->map_flags;
 
     cmintab->num_columns = (uint16_t)(attr->max_entries & 0x0000FFFF);
     cmintab->num_rows    = (uint16_t)(attr->max_entries >> 16);
 
-printf("%d,%d\n",cmintab->map.value_size, sizeof(counter_t));
-    
     /* check sanity of attributes. */
     if (cmintab->map.max_entries == 0 || cmintab->map.key_size == 0 ||
         cmintab->map.value_size != sizeof(counter_t) || 
         cmintab->num_rows == 0 || cmintab->num_rows == 0)
         goto free_cmintab;
 
-    /* creating table. */
-    cmintab->cmin_table = calloc(cmintab->num_rows, sizeof(counter_t*));
-    if (!cmintab->cmin_table)
+
+    cmintab->cmintab = calloc(cmintab->num_rows, sizeof(ght_hash_table_t*));
+    if (!cmintab->cmintab)
         goto free_cmintab;
 
     for (i = 0; i < cmintab->num_rows; i++) {   
-        cmintab->cmin_table[i] = calloc(cmintab->num_columns, sizeof(counter_t));
-        if (!cmintab->cmin_table[i]) {
+        cmintab->cmintab[i] = ght_create(cmintab->num_columns);
+        if (!cmintab->cmintab[i]) 
             goto free_cmintab;
-        }
     }
 
-    /* generating hash coefficients. */
-    cmintab->as = calloc(cmintab->num_rows, sizeof(uint8_t));
-    if (!cmintab->as) {
-        goto free_cmintab;
-    }
-    cmintab->bs = calloc(cmintab->num_rows, sizeof(uint8_t));
-    if (!cmintab->bs) {
-        goto free_cmintab;
-    }
-    for (i = 0; i < cmintab->num_rows; i++) {
-        cmintab->as[i] = rand();
-        cmintab->bs[i] = rand();
-    }
-
-    // cmintab->elem_size = sizeof(struct cmin_elem) +
-    //                      round_up(cmintab->map.key_size, 8) +
-    //                      round_up(cmintab->map.value_size, 8);
+    cmintab->elem_size = sizeof(struct cmin_elem) +
+                         round_up(cmintab->map.key_size, 8) +
+                         round_up(cmintab->map.value_size, 8);
 
     return &cmintab->map;
 
@@ -124,21 +91,22 @@ free_cmintab:
 // query in count-min sketch
 void *cmin_map_lookup_elem(struct bpf_map *map, void *key) {
     struct bpf_cmintab *cmintab = container_of(map, struct bpf_cmintab, map);
-    int i, col;
-    // struct cmin_elem *e;
+    int i;
+    struct cmin_elem *e;
     counter_t *min, *curr;
 
     /* finds min value of the entries in the tables */
-    col = hash(cmintab->as[0], cmintab->bs[0], key, cmintab->map.key_size);
-    col = col % cmintab->num_columns;
-    min = (counter_t *) (&cmintab->cmin_table[0][col]);
+    e = ght_get(cmintab->cmintab[0], map->key_size, key);
+    min = (counter_t *) (e->key + round_up(map->key_size, 8));
     for (i = 1; i < cmintab->num_rows; i++) {
-        col = hash(cmintab->as[i], cmintab->bs[i], key, cmintab->map.key_size);
-        col = col % cmintab->num_columns;
-        curr = (counter_t *) (&cmintab->cmin_table[i][col]);
-        if ((*curr) < (*min)) {
-            min = curr;
+        e = ght_get(cmintab->cmintab[i], map->key_size, key); 
+        if (!e) {
+            errno = ENOENT;
+            return NULL;
         }
+        curr = (counter_t *) (e->key + round_up(map->key_size, 8));
+        if ((*curr) < (*min))
+            min = curr;
     }
 
     return min;
@@ -149,14 +117,43 @@ void *cmin_map_lookup_elem(struct bpf_map *map, void *key) {
  */
 int cmin_map_update_elem(struct bpf_map *map, void *key, void *value, uint64_t map_flags) {
     struct bpf_cmintab *cmintab = container_of(map, struct bpf_cmintab, map);
-    int i, col;
-    // counter_t *value;
+    int i, err;
+    struct cmin_elem *e_old, *e_new;
+    counter_t *old_val, *new_val;
 
-    // updating each row entry (increasing counters)
+    new_val = calloc(1, sizeof(counter_t));
+    if (!new_val) {
+        errno = ENOMEM;
+        return -1;
+    }
+
+    // updating each row entry
     for (i = 0; i < cmintab->num_rows; i++) {
-        col = hash(cmintab->as[i], cmintab->bs[i], key, cmintab->map.key_size);
-        col = col % cmintab->num_columns;
-        cmintab->cmin_table[i][col] = cmintab->cmin_table[i][col] + 1;
+        // ght_replace doesn't work
+        e_old = ght_get(cmintab->cmintab[i], map->key_size, key);
+        if (e_old) {
+            old_val = (counter_t *) (e_old->key + round_up(map->key_size, 8));
+            memcpy(new_val, old_val, sizeof(counter_t));
+            ght_remove(cmintab->cmintab[i], map->key_size, key);
+            free(e_old);
+        } else {
+
+        }
+        (*new_val) += 1;
+
+        // Allocate the new element
+        e_new = calloc(1, cmintab->elem_size);
+        if (!e_new) {
+            errno = ENOMEM;
+            return -1;
+        }
+
+        memcpy(e_new->key, key, map->key_size);
+        memcpy(e_new->key + round_up(map->key_size, 8), new_val, map->value_size);
+
+        err = ght_insert(cmintab->cmintab[i], e_new->key, map->key_size, e_new);
+        if (err)
+            return err; 
     }
 
     return 0;
@@ -169,12 +166,17 @@ int cmin_map_delete_elem(struct bpf_map *map, void *key) {
 
 void cmin_map_free(struct bpf_map *map) {
     struct bpf_cmintab *cmintab = container_of(map, struct bpf_cmintab, map);
+    ght_iterator_t iterator;
+    const void *p_key;
+    void *p_e;
     int i;
 
     for (i = 0; i < cmintab->num_rows; i++) {
-        free(cmintab->cmin_table[i]);
+        for (p_e = ght_first(cmintab->cmintab[i], &iterator, &p_key); p_e; p_e = ght_next(cmintab->cmintab[i], &iterator, &p_key)) {
+            free(p_e);
+        }
+        ght_finalize(cmintab->cmintab[i]);
     }
-    free(cmintab->cmin_table);
     free(cmintab);
 }
 
